4221d020f37aaca647a5e8f6b5eba905883c2dd4
tuphan-dn <tuphan@descartes.network>
2022-06-21 18:11:49 +0700

    update default pair

--- src/app/view/index.tsx
+++ src/app/view/index.tsx
@@ -11,7 +11,7 @@ const View = () => {
     <EmbededView
       appId={appId}
       src={
-        'https://saros.finance/swap?base=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&pair=SENBBKVCM7homnf5RX9zqpf1GFe935hnbU4uVzY1Y6M'
+        'https://saros.finance/swap?base=C98A4nkJXhpVZNAZdHUA95RpTF3T4whtQubL3YobiUX9&pair=SENBBKVCM7homnf5RX9zqpf1GFe935hnbU4uVzY1Y6M'
       }
       title="Saros: A DeFi Super-Network Built on S◎lana."
       background={{ light: '#ffffff', dark: '#3c3b3b' }}
