b0af68d909a617ccfa51b1ccb6c0ace7508e8038
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-10-13 13:51:47 +0700

    Update senhub

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -20,7 +20,7 @@ const View = () => {
   if (net !== 'mainnet') return <MainnetOnly />
   return (
     <EmbededView
-      wallet={window.sentre.wallet}
+      wallet={window.sentre.solana}
       appId={appId}
       src={
         'https://saros.finance/swap?base=C98A4nkJXhpVZNAZdHUA95RpTF3T4whtQubL3YobiUX9&pair=SENBBKVCM7homnf5RX9zqpf1GFe935hnbU4uVzY1Y6M'
